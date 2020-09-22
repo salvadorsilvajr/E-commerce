bf8dcc8817e516f943299b8751f301e1ab430c07
salvadorsilvajr <salvadorsilvajr@yahoo.com>
2020-09-21 19:26:21 -0500

    done L25 all routes and controles for the auth

---
+++ app.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const expressValidator = require('express-validator');
+const dotenv = require('dotenv');
+dotenv.config();
+
+// import routes
+const authRoutes = require('./routes/auth');
+
+// app
+const app = express();
+
+
+//db connection
+mongoose.connect(
+      process.env.MONGO_URI, {
+         useNewUrlParser: true,
+         useCreateIndex: true,
+         useUnifiedTopology: true
+      }
+   )
+   .then(() => console.log('DB Connected...'))
+
+mongoose.connection.on('error', err => {
+   console.log(`DB connection error: ${err.message}`)
+});
+
+// middleware 
+app.use(bodyParser.json());
+app.use(morgan('dev'));
+app.use(cookieParser());
+app.use(expressValidator());
+
+
+// routes middleware
+app.use('/api', authRoutes);
+
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => {
+   console.log(`Server is running pn port ${port}`);
+});
